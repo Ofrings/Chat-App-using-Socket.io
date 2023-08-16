754b0209e7ed951bf53fbc530a3a044660f4d930
Omar SF <omarsanabriaf2013@gmail.com>
2023-08-15 23:52:43 -0600

    Acklen Avenue Challenge Fix

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -18,13 +18,9 @@ resource "aws_lb_listener" "omar-lb-listener" {
   port              = 80
   protocol          = "HTTP"
   
-  default_action {
-    type             = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      status_code  = "200"
-      message_body = "Testing load balance"
-    }
+    default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.target-group.id
   }
 
 }
@@ -54,4 +50,28 @@ resource "aws_security_group" "OmarGroup" {
   }
  
 }
+
+resource "aws_lb_target_group" "target-group" {
+  name     = "TargetGroupLB"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "instance"
+}
+
+
+resource "aws_lb_target_group_attachment" "lb_attach_a" {
+  target_group_arn = aws_lb_target_group.target-group.arn
+  target_id        = aws_instance.ec2_instance_1.id
+  
+}
+
+resource "aws_lb_target_group_attachment" "lb_attach_b" {
+  target_group_arn = aws_lb_target_group.target-group.arn
+  target_id        = aws_instance.ec2_instance_2.id
+  
+}
+
+
+
 
